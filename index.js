03907f15c10feb420b33b66f221913d58e91c60c
mbish08 <mdrew8@gmail.com>
2021-03-08 14:32:29 -0800

    commit tree a66c6f00a058d85b29b975afd89e684e5aee5284

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(x) {
+    while (x > 0) {
+        console.log(x);
+        x-= 1;
+    }
+    console.log( x );
+}
